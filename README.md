d52797449e3ed802088f7eabb96611c53a64f215
Daniil Domanetskii <daniil.domanetskii@jetruby.com>
2022-11-30 23:23:13 +0300

    5.6.1 Practical work: Learning how to work with the interactive Rails console. Description of basic CRUD queries

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # README
 
 Prepare config-files:
-
 `cp .env.example .env`
+
+### Practical work 5.6.1
+2.1) Post.all
+2.2) Post.order(created_at: :desc)
+2.3) Post.order(:created_at).limit(5)
+2.4) Post.find(1)
+2.5) Post.create(description: FFaker::Book.description)
+2.6) Post.first.update(description: 'description')
+2.7) Post.where('created_at <= ?', 5.minutes.ago).destroy_all
+
+5)
+2.times { User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name) }
+Post.create(user_id: 1, description: FFaker::Book.description)
+Post.create(user_id: 2, description: FFaker::Book.description)
+6)
+2.times { User.create(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name) }
+7)
+User.includes(:posts).references(:posts).where('posts.id IS NOT NULL')
+8)
+User.includes(:posts).references(:posts).where('posts.id IS NULL')
+9)
+User.includes(:posts)
+User.preload(:posts)
+User.eager_load(:posts)
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_22_194408) do
+ActiveRecord::Schema.define(version: 2022_11_30_190458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "posts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
+    t.string "first_name", null: false
     t.string "last_name"
     t.date "birthdate"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone_number", limit: 15, default: "11111111111", null: false
+    t.string "email"
   end
 
 end
